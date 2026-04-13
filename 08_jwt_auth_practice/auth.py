2e17568e5e37ec81c4266e0b52162c444d8f6131
Tanase Sergiu <tanasesergiu15@gmail.com>
2026-04-13 16:43:24 +0300

    Add JWT authentication functions and database setup

---
+++ 08_jwt_auth_practice/auth.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+SECRET_KEY = os.environ.get("SECRET_KEY ")
+
+ALGORITHM = "HS256"
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + \
+        timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+
+def verify_access_token(token: str) -> str:
+    try:
+        # verifies the signature and expiry automatically
+        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+
+        email = payload.get("sub")
+
+        if email is None:
+            raise JWTError
+
+        return email
+    except JWTError:
+        raise JWTError
